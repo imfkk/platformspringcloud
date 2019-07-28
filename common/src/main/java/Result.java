b9b3c180ad0277c1f801e74586dd558bbfb2e759
fkk <1091900529@qq.com>
2019-07-28 22:50:24 +0800

    添加公共模块

---
+++ common/src/main/java/Result.java
@@ -0,0 +1,34 @@
+import java.io.Serializable;
+
+public class Result<T> implements Serializable {
+        /*错误码*/
+        private Integer code;
+        /*提示信息*/
+        private String msg;
+        /*具体的内容*/
+        private T data;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ common/src/main/java/ResultUtil.java
@@ -0,0 +1,17 @@
+public class ResultUtil {
+    public static Result success(Object object) {
+        Result result = new Result();
+        result.setCode(0);
+        // result.setMsg(msg);
+        result.setData(object);
+        return result;
+    }
+
+    public static Result error(Integer code, String msg) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+
+}
