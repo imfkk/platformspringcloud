010aec465d32ae26300b6e869e18aab968de3ce9
fkk <1091900529@qq.com>
2019-07-29 23:15:57 +0800

    mybatis逆向工程 版本降级

---
+++ carsystem/src/main/java/com/kunluntop/carsystem/controller/CarManagerController.java
@@ -0,0 +1,24 @@
+package com.kunluntop.carsystem.controller;
+
+import com.kunluntop.carsystem.service.CarManagerService;
+import com.kunluntop.response.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping(value = "/cars")
+public class CarManagerController {
+    @Autowired
+    private CarManagerService carManagerService;
+
+    @RequestMapping(value = "/ListCars" ,method = RequestMethod.GET)
+    public Result ListCars() {
+        return carManagerService.list();
+    }
+
+   /* @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public com.kunluntop.response.Result addCars() {
+        return carManagerService.insert();
+    }*/
+
+}
---
+++ carsystem/src/main/java/com/kunluntop/carsystem/mapper/TbCarMapper.java
@@ -0,0 +1,30 @@
+package com.kunluntop.carsystem.mapper;
+
+import com.kunluntop.carsystem.pojo.TbCar;
+import com.kunluntop.carsystem.pojo.TbCarExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbCarMapper {
+    int countByExample(TbCarExample example);
+
+    int deleteByExample(TbCarExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TbCar record);
+
+    int insertSelective(TbCar record);
+
+    List<TbCar> selectByExample(TbCarExample example);
+
+    TbCar selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TbCar record, @Param("example") TbCarExample example);
+
+    int updateByExample(@Param("record") TbCar record, @Param("example") TbCarExample example);
+
+    int updateByPrimaryKeySelective(TbCar record);
+
+    int updateByPrimaryKey(TbCar record);
+}
