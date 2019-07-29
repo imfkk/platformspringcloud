5cc3a266897e08b6049e5a19a75e7629feb2eb67
fkk <1091900529@qq.com>
2019-07-30 00:07:55 +0800

    添加feign 工程

---
+++ carsystem-api/src/main/java/fallback/CarClientServiceFallbackFactory.java
@@ -0,0 +1,18 @@
+package fallback;
+
+import api.CarClientService;
+import com.kunluntop.response.Result;
+import com.kunluntop.response.ResultUtil;
+import feign.hystrix.FallbackFactory;
+
+public class CarClientServiceFallbackFactory implements FallbackFactory<CarClientService> {
+    @Override
+    public CarClientService create(Throwable throwable) {
+            return  new CarClientService() {
+                @Override
+                public Result getCarList() {
+                    return ResultUtil.error(-1,"调用接口失败");
+                }
+            };
+    }
+}
