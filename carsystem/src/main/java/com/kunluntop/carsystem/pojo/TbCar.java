ab95772dd9c30ca1eafbea17dc85923c5713ef1b
fkk <1091900529@qq.com>
2019-07-30 10:49:25 +0800

    pojo

---
+++ carsystem/src/main/java/com/kunluntop/carsystem/pojo/TbCar.java
@@ -0,0 +1,23 @@
+package com.kunluntop.carsystem.pojo;
+
+public class TbCar {
+    private Integer id;
+
+    private String plateNumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber == null ? null : plateNumber.trim();
+    }
+}
