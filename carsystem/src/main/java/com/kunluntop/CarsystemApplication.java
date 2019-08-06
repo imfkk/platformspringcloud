fde5c1b152a4d834ab373360cd91b3e821e7e85d
fkk <1091900529@qq.com>
2019-08-06 22:11:39 +0800

    豪猪

--- carsystem/src/main/java/com/kunluntop/CarsystemApplication.java
+++ carsystem/src/main/java/com/kunluntop/CarsystemApplication.java
@@ -4,11 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan("com.kunluntop.mapper")
-/*@EnableFeignClients("com.kunluntop.api.cars")*/
+@EnableFeignClients
+@EnableHystrix
 //@ComponentScan("com.kunluntop.api.cars")
 //@ComponentScan({"com.kunluntop"})
 public class CarsystemApplication {
