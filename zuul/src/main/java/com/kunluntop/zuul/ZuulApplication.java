b880f1488e2ad68337b38f37e0de1728dad361a3
fkk <1091900529@qq.com>
2019-08-07 22:06:08 +0800

    zuul

--- zuul/src/main/java/com/kunluntop/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/kunluntop/zuul/ZuulApplication.java
@@ -2,8 +2,10 @@ package com.kunluntop.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulApplication {
 
     public static void main(String[] args) {
